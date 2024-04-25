78c2cff244283a802101de119ecb493f8bec8523
Patrick Ružman <patrick.ruzman@bozic.io>
2024-04-25 17:57:23 +0200

    - [ Add ] - present decoded values in list view (legacy version -> new is recycle view )

--- Top10Apps/app/src/main/java/com/patrickr/top10apps/MainActivity.kt
+++ Top10Apps/app/src/main/java/com/patrickr/top10apps/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.patrickr.top10apps
 
+import android.app.LauncherActivity.ListItem
+import android.content.Context
 import android.os.AsyncTask
-import android.os.Build.VERSION_CODES.O
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import java.net.URL
+import com.patrickr.top10apps.databinding.ActivityMainBinding
+import com.patrickr.top10apps.databinding.ListItemBinding
+import kotlin.properties.Delegates
 
 class FeedEntry {
 	var name: String = ""
@@ -26,11 +33,14 @@ class FeedEntry {
 
 class MainActivity : AppCompatActivity() {
 	private val TAG = "MainActivity"
+	private lateinit var activityMain: ActivityMainBinding
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		activityMain = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(activityMain.root)
 		Log.d(TAG, "onCreate called")
-		val downloadData = DownloadData()
+		val downloadData = DownloadData(this, activityMain.xmlListView)
 		downloadData.execute(
 			"http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml"
 		)
@@ -38,8 +48,16 @@ class MainActivity : AppCompatActivity() {
 	}
 
 	companion object {
-		private class DownloadData : AsyncTask<String, Void, String>() {
+		private class DownloadData(context: Context, listView: ListView) : AsyncTask<String, Void, String>() {
 			private val TAG = "DownloadData"
+			var propContext: Context by Delegates.notNull()
+			var propListView: ListView by Delegates.notNull()
+
+			init {
+				propContext = context
+				propListView = listView
+			}
+
 			override fun doInBackground(vararg params: String?): String {
 				Log.d(TAG, "doInBackground: parameter is ${params[0]}")
 				val param = params[0] ?: return ""
@@ -57,62 +75,15 @@ class MainActivity : AppCompatActivity() {
 				if (result != null) {
 					parseApplications.parse(result)
 				}
+				val arrayAdapter = ArrayAdapter<FeedEntry>(propContext, R.layout.list_item, parseApplications.applications)
+				propListView.adapter = arrayAdapter
 			}
 
 			private fun downloadXML(urlPath: String): String {
 				return URL(urlPath).readText()
 			}
 
-//			private fun downloadXML(urlPath: String): String {
-//				val xmlResult = StringBuilder()
-//				try {
-//					val url = URL(urlPath)
-//					val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-//					val response = connection.responseCode
-//					Log.d(TAG, "Response code: $response")
-//					val reader = BufferedReader(
-//						InputStreamReader(connection.inputStream)
-//					)
-//
-//					val inputBuffer = CharArray(500)
-//					var charsRead = 0
-//					while (charsRead >= 0) {
-//						charsRead = reader.read(inputBuffer)
-//						if (charsRead > 0) {
-//							xmlResult.append(String(inputBuffer, 0, charsRead))
-//						}
-//					}
-//					reader.close()
-//					val stream = connection.inputStream
-//					stream.buffered().reader().use {
-//						xmlResult.append(it.readText())
-//					}
-//
-//					Log.d(TAG, "Received ${xmlResult.length} bytes")
-//					return xmlResult.toString()
-//				} catch (error: MalformedURLException) {
-//					Log.e(TAG, "downloadXML: Invalid URL: ${error.message}")
-//				} catch (error: IOException) {
-//					Log.e(TAG, "downloadXML: IO Exception reading data: ${error.message}")
-//				} catch (error: SecurityException) {
-//					error.printStackTrace()
-//					Log.e(TAG, "downloadXML: Security Exception: ${error.message}")
-//				} catch (error: Exception) {
-//					Log.e(TAG, "downloadXML: Unknown error occurred!")
-//				}
-//				} catch (error: Exception) {
-//					val errorMessage = when (error) {
-//						is MalformedURLException -> "downloadXML: Invalid URL: ${error.message}"
-//						is IOException -> "downloadXML: IO Exception reading data: ${error.message}"
-//						is SecurityException -> "downloadXML: Security Exception: ${error.message}"
-//						else -> "downloadXML: Unknown error occurred!"
-//					}
-//
-//					Log.e(TAG, errorMessage)
-//				}
-//
-//				return ""
-//			}
+
 		}
 	}
 }
