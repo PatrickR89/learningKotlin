ae1ab70b028b19c97e6cb7bbe6812abf2b8d2896
Patrick Ružman <patrick.ruzman@bozic.io>
2024-08-21 13:02:11 +0200

    FirestoreProject - [ Add ] - loading data

--- FireStoreProject/app/src/main/java/com/patrickr/firestoreproject/MainActivity.kt
+++ FireStoreProject/app/src/main/java/com/patrickr/firestoreproject/MainActivity.kt
@@ -3,16 +3,21 @@ package com.patrickr.firestoreproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 	private lateinit var editTextTitle: AppCompatEditText
 	private lateinit var editTextDescription: AppCompatEditText
 	private lateinit var saveButton: AppCompatButton
+	private lateinit var loadButton: AppCompatButton
+	private lateinit var textView: TextView
 	private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+	private val docRef: DocumentReference = MainActivity.getDocumentRef(db)
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -28,6 +33,53 @@ class MainActivity : AppCompatActivity() {
 		saveButton.setOnClickListener {
 			saveNote()
 		}
+
+		loadButton = findViewById(R.id.load_button)
+		textView = findViewById(R.id.text_view_data)
+		loadButton.setOnClickListener {
+			loadNote()
+		}
+	}
+
+	override fun onStart() {
+		super.onStart()
+		docRef.addSnapshotListener { value, error ->
+			error?.let {
+				return@addSnapshotListener
+			}
+
+			value?.let {
+				require(it.exists())
+				val title = it.getString(DatabaseKeys.title.name)
+				val description = it.getString(DatabaseKeys.description.name)
+				textView.text = "Title: $title. \nDescription: $description"
+			}
+		}
+	}
+
+	private fun loadNote() {
+		docRef
+			.get()
+			.addOnSuccessListener { document ->
+				require(document.exists())
+				val title = document.getString(DatabaseKeys.title.name)
+				val description = document.getString(DatabaseKeys.description.name)
+
+				val note = mutableMapOf<String, Any>()
+
+				title?.let {
+					note[DatabaseKeys.title.name] = it
+				}
+
+				description?.let {
+					note[DatabaseKeys.description.name] = it
+				}
+
+				textView.text = "Title: $title. \nDescription: $description"
+			}
+			.addOnFailureListener {
+				Toast.makeText(this@MainActivity, "Failed to load.", Toast.LENGTH_LONG).show()
+			}
 	}
 
 	private fun saveNote() {
@@ -38,9 +90,7 @@ class MainActivity : AppCompatActivity() {
 		note[DatabaseKeys.title.name] = title
 		note[DatabaseKeys.description.name] = description
 
-		db
-			.collection("Collection")
-			.document("My first note")
+		docRef
 			.set(note)
 			.addOnSuccessListener {
 				Toast.makeText(this@MainActivity, "Note added!", Toast.LENGTH_LONG).show()
@@ -50,6 +100,15 @@ class MainActivity : AppCompatActivity() {
 			.addOnFailureListener {
 				Toast.makeText(this@MainActivity, "Note not added!", Toast.LENGTH_LONG).show()
 			}
+	}
+
+	companion object {
+		val collectionName: String = "Notebook"
+		val documentName: String = "My first note"
+
+		fun getDocumentRef(db: FirebaseFirestore): DocumentReference {
+			return db.collection(collectionName).document(documentName)
+		}
 	}
 }
 
